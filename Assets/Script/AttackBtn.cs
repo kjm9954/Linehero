e483c66c6857b4d6c94412aec1b9b0ab760fdeae
TrueKnowledge <goust277@gmail.com>
2023-11-01 12:36:31 +0900

    화면 페이드 적용

--- Assets/Script/AttackBtn.cs
+++ Assets/Script/AttackBtn.cs
@@ -20,6 +20,7 @@ public class AttackBtn : MonoBehaviour
     public GameObject spn;
     public GameObject resultime;
     public GameObject Stage;
+    public GameObject Back;
     public float wating = 0;
     GameObject Left = null;
     GameObject Right = null;
@@ -162,6 +163,7 @@ public class AttackBtn : MonoBehaviour
         if (InGame.GetComponent<InGameManager>().MonsterNum == 0)
         {
             spn.GetComponent<SpineAnimation>().Clear();
+            Back.GetComponent<Fade>().ShowBack();
             Stage.GetComponent<Stage_Clear>().Stage();
             Invoke("ClearStage", resultime.GetComponent<Result>().ClearTime);
         }
